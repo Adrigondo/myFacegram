8000dfab4264d475efff40f88febb0bb5e4d44b8
Adora <im.stariluz@gmail.com>
2024-06-22 12:26:37 -0600

    Fix bug #1 Fixing static root and updating static url to github pages

--- myFacegram/settings_production.py
+++ myFacegram/settings_production.py
@@ -132,12 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-
-STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_URL = 'https://stariluz.github.io/myFacegram/'
 STATICFILES_DIRS = [
     (BASE_DIR / 'static'),
 ]
