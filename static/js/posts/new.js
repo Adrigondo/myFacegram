b97caa5f24666ba706282c87e921b10a94ed6c45
Adora <im.stariluz@gmail.com>
2024-07-19 00:56:50 -0600

    Fix #9 Contenteditable fields working on form save

--- static/js/posts/new.js
+++ static/js/posts/new.js
@@ -1,16 +1,15 @@
-// new.js
 let $title_editable = document.getElementById("title__editable");
 let $title = document.getElementById("title");
 let $content_text_editable = document.getElementById("content_text__editable");
 let $content_text = document.getElementById("content_text");
 
-
-$title.addEventListener("input", function() {
+$title_editable.addEventListener("input", function() {
+    console.log("HOLA");
     let title = $title_editable.textContent;
     $title.value=title;
 }, false);
 
-$content_text.addEventListener("input", function() {
+$content_text_editable.addEventListener("input", function() {
     let content_text = $content_text_editable.textContent;
     $content_text.value=content_text;
 }, false);
