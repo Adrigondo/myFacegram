22067ae521e0ad4d2a06e868a805d280ac9e7a01
Adora <im.stariluz@gmail.com>
2024-07-20 01:39:48 -0600

    Fix #10 Setting production static url to github again

--- myFacegram/settings_production.py
+++ myFacegram/settings_production.py
@@ -151,7 +151,7 @@ USE_TZ = True
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATIC_URL = f'{STATIC_ROOT}/'
+STATIC_URL = 'https://stariluz.github.io/myFacegram/'
 STATICFILES_DIRS = [
     (BASE_DIR / 'static'),
 ]
