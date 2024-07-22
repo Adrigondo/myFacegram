69e2167fe277cf966d58102ec30ad8a668ac8d48
Adora <im.stariluz@gmail.com>
2024-07-21 22:24:02 -0600

    Fix #10 Change masonry library

--- static/js/posts/masonry.js
+++ static/js/posts/masonry.js
@@ -1,7 +1,14 @@
+import MiniMasonry from '../modules/minimasonry.js';
+
 var $grid = document.querySelector('.posts-grid');
-var masonry = new Masonry($grid, {
-    // Opciones
-    itemSelector: '.post-container',
-    columnWidth: '.post-container',
-    percentPosition: true
+// var masonry = new Masonry($grid, {
+//     // Opciones
+//     itemSelector: '.post-container',
+//     columnWidth: '.post-container',
+//     percentPosition: true
+// });
+var masonry = new MiniMasonry({
+    container: $grid,
+    baseWidth: 250,
+    minify: true,
 });
