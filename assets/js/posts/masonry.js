40b260cfa4962c1d87edcb9531626a74a1e83f60
Adora <im.stariluz@gmail.com>
2024-07-21 22:45:40 -0600

    Collectstatic

--- assets/js/posts/masonry.js
+++ assets/js/posts/masonry.js
@@ -1,7 +1,11 @@
-var $grid = document.querySelector('.posts-grid');
-var masonry = new Masonry($grid, {
-    // Opciones
-    itemSelector: '.post-container',
-    columnWidth: '.post-container',
-    percentPosition: true
+import MiniMasonry from '../modules/minimasonry.js';
+
+const $grid = document.querySelector('.posts-grid');
+const masonry = new MiniMasonry({
+    container: $grid,
+    baseWidth: 250,
+    gutter: 0, 
+    minify: true,
 });
+
+$grid.querySelectorAll('img').forEach((img) => img.addEventListener('load', () => masonry.layout()))
